5b5cf09f12c7ce92102dcfc74068c9c49b1cb79c
owais-005 <owaisansarcodm@gmail.com>
2025-06-25 13:55:10 +0500

    Update uploadMiddleware.ts

--- middleware/uploadMiddleware.ts
+++ middleware/uploadMiddleware.ts
@@ -2,7 +2,6 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// ✅ Ensure the 'uploads/images' directory exists
 const uploadDir = "uploads/images/";
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
