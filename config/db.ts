706ec30c8a663f2a6efc887accd2fb81654717c4
owais-005 <owaisansarcodm@gmail.com>
2025-06-25 13:53:17 +0500

    Update db.ts

--- config/db.ts
+++ config/db.ts
@@ -1,17 +1,3 @@
-// import mysql from "mysql2/promise";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: process.env.DB_HOST || "147.93.122.109",
-//   user: process.env.DB_USER || "tech_UseRMemEentS",
-//   password: process.env.DB_PASSWORD || "tech_UseRMemEentS",
-//   database: process.env.DB_NAME || "tech_MemberEvents",
-// });
-
-// export default pool;
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
